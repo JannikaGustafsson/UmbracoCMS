041d2fb76296c16d940a930ff62b9f560902dd89
Jannika Gustafsson <jannikagustafsson@gmail.com>
2025-11-12 13:10:15 +0100

    add callbacksection and more

---
+++ UmbracoCMSProject/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoCMSProject.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+
+
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$", ErrorMessage = "Invalid email address.")]
+
+    public string Email { get; set; } = null!;
+
+
+    [Required(ErrorMessage = "Phone is required")]
+    [Display(Name = "Phone")]
+    public string Phone {  get; set; } = null!;
+
+
+    [Required(ErrorMessage = "Please select an option")]
+    public string SelectedOption { get; set; } = null!;
+
+    [BindNever]
+    public IEnumerable<string> Options { get; set; } = [];
+}
